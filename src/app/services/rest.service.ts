461759a8226142f34860e5a243aae70afe24e9b6
Pablo <grupodixis@gmail.com>
2018-09-14 19:16:10 +0200

    Pagina de administracion

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 export class RestService {
 
   constructor(private http: HttpClient) {}
-getRest(url) {
+getRest(url): any {
   return this.http.get(url);
 }
 
